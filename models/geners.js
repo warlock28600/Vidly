ecbd902e88a96fa2ee5dd38f4e826fb2eda55af3
Farhang <farhang.ezziirani@gmail.com>
2023-12-07 01:30:53 +0330

    some work

---
+++ models/geners.js
@@ -0,0 +1,25 @@
+const mongoose=require('mongoose')
+const Joi=require('joi')
+
+
+
+
+const generSchema=new mongoose.Schema({
+    name:String,
+    date:{type:Date,default:Date.now}
+  })
+  
+  const Gener=mongoose.model('Gener',generSchema)
+
+function validateGener(gener){
+    const schema={
+        name:Joi.string().required().min(5).max(25)
+    };
+
+    return schema.validate(gener)
+}
+
+
+exports.Gener=Gener
+exports.validate=validateGener;
+
--- routes/v1/genres.js
+++ routes/v1/genres.js
@@ -1,48 +1,16 @@
 const express = require("express");
-const { string, date } = require("joi");
+const {Gener,validate}=require('../../models/geners')
 const router = express.Router();
 const mongoose=require('mongoose')
 mongoose.connect('mongodb://127.0.0.1/Vidly')
 .then(()=>{console.log('connected to database....')})
 .catch(err=>{console.error('could not connect to database',err)})
 
-const generSchema=new mongoose.Schema({
-  name:String,
-  date:{type:Date,default:Date.now}
-})
-
-const Gener=mongoose.model('Gener',generSchema)
-
-
-async function craeteGener(){
-  const gener=new Gener({
-    name:'Deram'
-  })
-  
-  const result=await gener.save()
-  console.log(result)
-}
-
-
-async function getGener(){
-  const geners=await Gener.find();
-  console.log(geners)
-}
-
-getGener()
-
-
-
-const genres = [
-  { id: 1, name: "Action" },
-  { id: 2, name: "Deram" },
-  { id: 3, name: "thriler" },
-];
-
 router.use(express.json());
 
-router.get("/", (req, res) => {
-  res.send(genres);
+router.get("/", async (req, res) => {
+  const geners=await Gener.find()
+  res.send(geners);
 });
 
 router.get("/:id", (req, res) => {
