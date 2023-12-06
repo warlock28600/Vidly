33035d7acef9e18c22a76fb22c08edc53e8e853d
Farhang <farhang.ezziirani@gmail.com>
2023-12-07 00:22:33 +0330

    some work

--- routes/v1/genres.js
+++ routes/v1/genres.js
@@ -1,5 +1,37 @@
 const express = require("express");
+const { string, date } = require("joi");
 const router = express.Router();
+const mongoose=require('mongoose')
+mongoose.connect('mongodb://127.0.0.1/Vidly')
+.then(()=>{console.log('connected to database....')})
+.catch(err=>{console.error('could not connect to database',err)})
+
+const generSchema=new mongoose.Schema({
+  name:String,
+  date:{type:Date,default:Date.now}
+})
+
+const Gener=mongoose.model('Gener',generSchema)
+
+
+async function craeteGener(){
+  const gener=new Gener({
+    name:'Deram'
+  })
+  
+  const result=await gener.save()
+  console.log(result)
+}
+
+
+async function getGener(){
+  const geners=await Gener.find();
+  console.log(geners)
+}
+
+getGener()
+
+
 
 const genres = [
   { id: 1, name: "Action" },
@@ -34,5 +66,18 @@ router.post("/", (req, res) => {
   genres.push(gener);
   res.status(200).send(gener);
 });
+
+
+router.put('/:id',(req,res)=>{
+  const gener=genres.find((gener)=>{
+    return gener.id=req.params.id
+  })
+if(!gener){
+  res.status(400).send(`genre with id ${req.params.id} dose not exists`)
+}
+  gener.name=req.body.name;
+  res.status(200).send(gener)
+})
+
 
 module.exports = router;
