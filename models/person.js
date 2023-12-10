8f1a2996da5032d17717d3934e45b184cd6a86b2
Farhang <farhang.ezziirani@gmail.com>
2023-12-10 23:17:16 +0330

    some work

--- models/person.js
+++ models/person.js
@@ -16,7 +16,7 @@ const personSchema = new mongoose.Schema({
     },
     date: {
         type: Date,
-        default: Date.now()
+        default: Date.now
     },
     FatherName: {
         type: String,
